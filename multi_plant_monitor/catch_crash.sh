3ed7a3ee86345f3bd14711bb62e2bc277108f236
Jeff Yang <jyang1@gmail.com>
2026-06-30 22:20:37 -0500

    Remove diagnostic scripts and logs

--- multi_plant_monitor/catch_crash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true; do
-  if [ -c /dev/cu.usbserial-5B1F0101911 ]; then
-    stty -f /dev/cu.usbserial-5B1F0101911 115200 raw -clocal -echo
-    cat /dev/cu.usbserial-5B1F0101911 >> serial_crash.log
-  fi
-  sleep 0.1
-done
